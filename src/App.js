14829c3504b6e67e148730b4f19bb3f0d7e3a7b2
anastasiia-kolieva <nastya1110@gmail.com>
2020-12-29 19:08:16 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react';
 import Container from './components/Container/Container';
+import AppBar from './components/Appbar/Appbar';
 
 export default function App() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <AppBar />
+    </Container>
+  );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.css';
 
-export default function Navigation() {}
+export default function Navigation() {
+  return <div>Привет!</div>;
+}
