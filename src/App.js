43ea64dab9bbdae0c0fec6fbc6fe3af012925765
anastasiia-kolieva <nastya1110@gmail.com>
2020-12-29 18:50:22 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import { useState, useEffect } from 'react';
+// import s from './App.module.css';
+
+export default function App() {
+  return {
+    /* <Container>
+    
+</Container> */
+  };
+}
